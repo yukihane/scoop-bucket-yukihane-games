cc884fe7d2f8640cc6a65b442941da1f4d288d27
yukihane <yukihane.feather@gmail.com>
2018-09-06 21:18:24 +0900

    persistanceデータ初期化処理

---
+++ bin/init_persist.ps1
@@ -0,0 +1,21 @@
+if (!(Test-Path("$persist_dir"))) {
+  New-Item "$persist_dir" -type directory
+}
+if (!(Test-Path("$persist_dir\Hengband.INI"))) {
+  New-Item "$persist_dir\Hengband.INI" -type file
+}
+if (!(Test-Path("$persist_dir\apex"))) {
+  New-Item "$persist_dir\apex" -type directory
+}
+if (!(Test-Path("$persist_dir\bone"))) {
+  New-Item "$persist_dir\bone" -type directory 
+}
+if (!(Test-Path("$persist_dir\data"))) {
+  New-Item "$persist_dir\data" -type directory 
+}
+if (!(Test-Path("$persist_dir\save"))) {
+  New-Item "$persist_dir\save" -type directory 
+}
+if (!(Test-Path("$persist_dir\user"))) {
+  New-Item "$persist_dir\user" -type directory 
+}
