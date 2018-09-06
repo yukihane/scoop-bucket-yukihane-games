9798593019a0b9ca4530769f387642e616bcdece
yukihane <yukihane.feather@gmail.com>
2018-09-06 23:08:32 +0900

    重複処理をまとめる

--- bin/hengband_init_persist.ps1
+++ bin/hengband_init_persist.ps1
@@ -1,23 +1,17 @@
 Param($execname)
 
+.\bin\hengband_persist_dirs.ps1
+
 if (!(Test-Path("$persist_dir"))) {
   New-Item "$persist_dir" -type directory
 }
-if (!(Test-Path("$persist_dir\Hengband.INI"))) {
+
+if (!(Test-Path("$persist_dir\$execname.INI"))) {
   New-Item "$persist_dir\$execname.INI" -type file
 }
-if (!(Test-Path("$persist_dir\apex"))) {
-  New-Item "$persist_dir\apex" -type directory
-}
-if (!(Test-Path("$persist_dir\bone"))) {
-  New-Item "$persist_dir\bone" -type directory 
-}
-if (!(Test-Path("$persist_dir\data"))) {
-  New-Item "$persist_dir\data" -type directory 
-}
-if (!(Test-Path("$persist_dir\save"))) {
-  New-Item "$persist_dir\save" -type directory 
-}
-if (!(Test-Path("$persist_dir\user"))) {
-  New-Item "$persist_dir\user" -type directory 
+
+foreach($dirname in $persist_dirs) {
+  if (!(Test-Path("$persist_dir\$dirname"))) {
+    New-Item "$persist_dir\$dirname" -type directory
+  }
 }
